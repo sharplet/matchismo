14319c8a2e9ba8a12427b6b24e1a373dd902c64e
Adam Sharp <adam.sharp@aviarc.com.au>
2013-01-26 13:22:46 +1100

    Initial Commit

---
+++ CardGame/SCSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCSAppDelegate.h
+//  CardGame
+//
+//  Created by Adam Sharp on 26/01/13.
+//  Copyright (c) 2013 Adam Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CardGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CardGame
+//
+//  Created by Adam Sharp on 26/01/13.
+//  Copyright (c) 2013 Adam Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCSAppDelegate class]));
+    }
+}
