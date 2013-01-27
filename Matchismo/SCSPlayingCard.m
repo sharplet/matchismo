aa790c2404d22ee04097d51dde793d4e4a0d5714
Adam Sharp <adam.sharp@aviarc.com.au>
2013-01-27 22:46:35 +1100

    Rename project to Matchismo

--- Matchismo/SCSPlayingCard.m
+++ Matchismo/SCSPlayingCard.m
@@ -1,6 +1,6 @@
 //
 //  SCSPlayingCard.m
-//  CardGame
+//  Matchismo
 //
 //  Created by Adam Sharp on 26/01/13.
 //  Copyright (c) 2013 Adam Sharp. All rights reserved.
--- Matchismo/main.m
+++ Matchismo/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  CardGame
+//  Matchismo
 //
 //  Created by Adam Sharp on 26/01/13.
 //  Copyright (c) 2013 Adam Sharp. All rights reserved.
