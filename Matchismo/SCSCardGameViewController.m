3574f7ed72770d8ecc5d197095588c09f3fce3a8
Adam Sharp <adam.sharp@aviarc.com.au>
2013-01-30 21:04:12 +1100

    Add main story board and view controller

---
+++ Matchismo/SCSCardGameViewController.m
@@ -0,0 +1,38 @@
+//
+//  SCSCardGameViewController.m
+//  Matchismo
+//
+//  Created by Adam Sharp on 30/01/13.
+//  Copyright (c) 2013 Adam Sharp. All rights reserved.
+//
+
+#import "SCSCardGameViewController.h"
+
+@interface SCSCardGameViewController ()
+
+@end
+
+@implementation SCSCardGameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
