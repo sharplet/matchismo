c4c99558608576e5bedde543f14ef2bb37dcf4fc
Adam Sharp <adam.sharp@outware.com.au>
2013-02-01 07:39:07 +1100

    Add card game model class with interface

---
+++ Matchismo/SCSCardMatchingGame.m
@@ -0,0 +1,18 @@
+//
+//  SCSCardMatchingGame.m
+//  Matchismo
+//
+//  Created by Adam Sharp on 1/02/13.
+//  Copyright (c) 2013 Adam Sharp. All rights reserved.
+//
+
+#import "SCSCardMatchingGame.h"
+
+@interface SCSCardMatchingGame()
+@property (readwrite, nonatomic) NSInteger score;
+@property (strong, nonatomic) NSMutableArray *cards;
+@end
+
+@implementation SCSCardMatchingGame
+
+@end
