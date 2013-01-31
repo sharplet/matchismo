1dc7cc2def310ca8e51620a49f2cb3b58134e7e8
Adam Sharp <adam.sharp@outware.com.au>
2013-02-01 08:03:28 +1100

    Implement card flipping and scoring logic

--- Matchismo/SCSCardMatchingGame.m
+++ Matchismo/SCSCardMatchingGame.m
@@ -8,6 +8,10 @@
 
 #import "SCSCardMatchingGame.h"
 
+#define FLIP_COST 1
+#define MATCH_BONUS 4
+#define MISMATCH_PENALTY 2
+
 @interface SCSCardMatchingGame()
 @property (readwrite, nonatomic) NSInteger score;
 @property (strong, nonatomic) NSMutableArray *cards;
@@ -40,6 +44,45 @@
         }
     }
     return self;
+}
+
+#pragma mark - Working with cards
+
+-(SCSCard *)cardAtIndex:(NSUInteger)index
+{
+    if (index < [self.cards count]) {
+        return self.cards[index];
+    }
+    else {
+        return nil;
+    }
+}
+
+-(void)flipCardAtIndex:(NSUInteger)index
+{
+    SCSCard *card = [self cardAtIndex:index];
+
+    if (!card.isUnplayable) {
+        // scoring happens whenever a card is flipped face up
+        if (card.isFaceUp) {
+            for (SCSCard *otherCard in self.cards) {
+                if (otherCard.isFaceUp && !otherCard.isUnplayable) {
+                    NSInteger matchScore = [card match:@[otherCard]];
+                    if (matchScore) {
+                        otherCard.unplayable = YES;
+                        card.unplayable = YES;
+                        self.score += matchScore * MATCH_BONUS;
+                    }
+                    else {
+                        otherCard.faceUp = NO;
+                        self.score -= MISMATCH_PENALTY;
+                    }
+                }
+            }
+            self.score -= FLIP_COST;
+        }
+        card.faceUp = !card.isFaceUp;
+    }
 }
 
 @end
