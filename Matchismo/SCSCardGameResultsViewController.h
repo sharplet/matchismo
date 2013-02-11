8779348c3abb24f4a191467a2d0c12692c57396d
Adam Sharp <adam.sharp@outware.com.au>
2013-02-11 19:47:29 +1100

    Create tab bar controller and empty scores screen

---
+++ Matchismo/SCSCardGameResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCSCardGameResultsViewController.h
+//  Matchismo
+//
+//  Created by Adam Sharp on 11/02/13.
+//  Copyright (c) 2013 Adam Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSCardGameResultsViewController : UIViewController
+
+@end
---
+++ Matchismo/SCSCardGameResultsViewController.m
@@ -0,0 +1,38 @@
+//
+//  SCSCardGameResultsViewController.m
+//  Matchismo
+//
+//  Created by Adam Sharp on 11/02/13.
+//  Copyright (c) 2013 Adam Sharp. All rights reserved.
+//
+
+#import "SCSCardGameResultsViewController.h"
+
+@interface SCSCardGameResultsViewController ()
+
+@end
+
+@implementation SCSCardGameResultsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
